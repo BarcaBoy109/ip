74a90772ca202e32b83110629e1b38873ff71671
Shehroz <shehrozawadulhoque@gmail.com>
2026-08-23 06:28:17 +0800

    Improve KOTHA banner art

--- src/main/java/Kotha.java
+++ src/main/java/Kotha.java
@@ -1,17 +1,17 @@
 public class Kotha {
     public static void main(String[] args) {
-        String banner = " _  __     ___    _____  _   _   _    \n"
-                + "| |/ /    / _ \\  |_   _|| | | | | |   \n"
-                + "| ' /    | | | |   | |  | |_| | | |   \n"
-                + "| . \\    | |_| |   | |  |  _  | | |   \n"
-                + "|_|\\_\\    \\___/    |_|  |_| |_| |_|   \n";
+        String banner = " _  __   ___  _____ _   _    _    \n"
+                + "| |/ /  / _ \\|_   _| | | |  / \\   \n"
+                + "| ' /  | | | | | | | |_| | / _ \\  \n"
+                + "| . \\  | |_| | | | |  _  |/ ___ \\ \n"
+                + "|_|\\_\\  \\___/  |_| |_| |_/_/   \\_\\\n";
         System.out.println(banner);
         System.out.println("____________________________________________________________");
         System.out.println(banner);
-        System.out.println("Hello! I'm KOTHA.");
-        System.out.println("What can I do for you?");
+        System.out.println("Hi Hi Hi! I'm KOTHA.");
+        System.out.println("What do you need?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
